a549b61b6bee7120db88d5a76b2c3e09a1e488f9
Kyuofox <kyuofox@gmail.com>
2023-07-05 19:10:58 +0800

    lmi: Update Hotword Enrollment blobs from V14.0.3.0.TJAEUXM
    Change-Id: I168385ea86698943d0614707cf5e41b798cd4c50
    Signed-off-by: Kyuofox <kyuofox@gmail.com>

--- lmi-vendor.mk
+++ lmi-vendor.mk
@@ -1398,8 +1398,8 @@ PRODUCT_PACKAGES += \
     TimeService \
     PowerOffAlarm \
     uimgbaservice \
-    HotwordEnrollmentOKGoogleHEXAGON \
-    HotwordEnrollmentXGoogleHEXAGON \
+    HotwordEnrollmentOKGoogleHEXAGON_WIDEBAND \
+    HotwordEnrollmentXGoogleHEXAGON_WIDEBAND \
     ImsRcsService \
     QtiTelephony \
     QtiTelephonyService \
