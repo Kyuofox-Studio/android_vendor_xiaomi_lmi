73af65e13929784800828ca7f7221af44c71aa03
Kyuofox <kyuofox@gmail.com>
2024-11-22 17:10:42 +0800

    lmi: Drop 32-bit camera HAL
    Change-Id: I5aecef15edfd00ad0c209f6e6814df15eec724f3
    Signed-off-by: Kyuofox <kyuofox@gmail.com>

--- lmi-vendor.mk
+++ lmi-vendor.mk
@@ -502,16 +502,6 @@ PRODUCT_PACKAGES += \
     libbt-hidlclient \
     libbtnv \
     libc2d30_bltlib \
-    libcamxexternalformatutils \
-    libcamxfacialfeatures \
-    libcamxfdalgo \
-    libcamxfdengine \
-    libcamxifestriping \
-    libcamximageformatutils \
-    libcamxncs \
-    libcamxstatscore \
-    libcamxswprocessalgo \
-    libcamxtintlessalgo \
     libcapiv2svacnn \
     libcapiv2svarnn \
     libcapiv2vop \
@@ -804,6 +794,16 @@ PRODUCT_PACKAGES += \
     libcamera_nn_stub \
     libcamera_scene \
     libcamerapostproc \
+    libcamxexternalformatutils \
+    libcamxfacialfeatures \
+    libcamxfdalgo \
+    libcamxfdengine \
+    libcamxifestriping \
+    libcamximageformatutils \
+    libcamxncs \
+    libcamxstatscore \
+    libcamxswprocessalgo \
+    libcamxtintlessalgo \
     libcdfw \
     libcdfw_remote_api \
     libchilog \
